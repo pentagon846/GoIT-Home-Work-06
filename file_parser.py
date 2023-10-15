3ba6ad080a8606087209574b97971e724ad35fca
pentagon846 <pentagon846@gmail.com>
2023-10-15 17:55:49 +0200

    Initial commit

---
+++ file_parser.py
@@ -0,0 +1,116 @@
+import sys
+from pathlib import Path
+from normalize import normalize
+import shutil
+
+PDF_FILE = []
+HTM_MY_DOCUMENTS = []
+GIF_IMAGES = []
+JPEG_IMAGES = []
+JPG_IMAGES = []
+PNG_IMAGES = []
+SVG_IMAGES = []
+MP3_AUDIO = []
+MY_OTHER = []
+ARCHIVES = []
+
+REGISTER_EXTENSION = {
+    'PDF': PDF_FILE,
+    'GIF': GIF_IMAGES,
+    'JPEG': JPEG_IMAGES,
+    'JPG': JPG_IMAGES,
+    'PNG': PNG_IMAGES,
+    'SVG': SVG_IMAGES,
+    'MP3': MP3_AUDIO,
+    'ZIP': ARCHIVES,
+    'HTM': HTM_MY_DOCUMENTS
+}
+
+FOLDERS = []
+EXTENSIONS = set()
+UNKNOWN = set()
+
+def get_extension(name: str) -> str:
+    return Path(name).suffix[1:].upper()  # suffix[1:] -> .jpg -> jpg
+
+def scan(folder: Path):
+    for item in folder.iterdir():
+        # Робота з папкою
+        if item.is_dir():  # перевіряємо чи обєкт папка
+            if item.name not in ('archives', 'video', 'audio', 'documents', 'images', 'MY_OTHER'):
+                FOLDERS.append(item)
+                scan(item)
+            continue
+
+        # Робота з файлом
+        extension = get_extension(item.name)  # беремо розширення файлу
+        full_name = folder / item.name  # беремо повний шлях до файлу
+        if not extension:
+            MY_OTHER.append(full_name)
+        else:
+            try:
+                ext_reg = REGISTER_EXTENSION[extension]
+                ext_reg.append(full_name)
+                EXTENSIONS.add(extension)
+            except KeyError:
+                UNKNOWN.add(extension)
+                MY_OTHER.append(full_name)
+
+
+def handle_image(file_name, target_folder):
+    target_folder.mkdir(exist_ok=True, parents=True)
+    normalized_name = normalize(file_name.stem) + file_name.suffix
+    file_name.replace(target_folder / normalized_name)
+
+
+def handle_video(file_name, target_folder):
+    target_folder.mkdir(exist_ok=True, parents=True)
+    normalized_name = normalize(file_name.stem) + file_name.suffix
+    file_name.replace(target_folder / normalized_name)
+
+
+def handle_document(file_name, target_folder):
+    target_folder.mkdir(exist_ok=True, parents=True)
+    normalized_name = normalize(file_name.stem) + file_name.suffix
+    file_name.replace(target_folder / normalized_name)
+
+
+def handle_audio(file_name, target_folder):
+    target_folder.mkdir(exist_ok=True, parents=True)
+    normalized_name = normalize(file_name.stem) + file_name.suffix
+    file_name.replace(target_folder / normalized_name)
+
+
+def handle_unknown(file_name, target_folder):
+    target_folder.mkdir(exist_ok=True, parents=True)
+    normalized_name = normalize(file_name.stem) + file_name.suffix
+    file_name.replace(target_folder / normalized_name)
+
+
+def handle_archive(file_name, target_folder):
+    target_folder.mkdir(exist_ok=True, parents=True)
+    new_name = normalize(file_name.stem)  # Видаляємо розширення, залишаючи тільки ім'я
+    new_folder = target_folder / new_name
+    new_folder.mkdir(exist_ok=True, parents=True)
+    try:
+        shutil.unpack_archive(str(file_name), str(new_folder))
+    except shutil.ReadError:
+        print(f'Unable to unpack archive: {file_name}')
+    else:
+        file_name.unlink()
+
+if __name__ == '__main__':
+    folder_process = sys.argv[1]
+    scan(Path(folder_process))
+    print((f'Images gif: {GIF_IMAGES}'))
+    print(f'Images jpeg: {JPEG_IMAGES}')
+    print(f'Images jpg: {JPG_IMAGES}')
+    print(f'Images png: {PNG_IMAGES}')
+    print(f'AUDIO mp3: {MP3_AUDIO}')
+    print(f'Archives zip: {ARCHIVES}')
+
+    print(f'EXTENSIONS: {EXTENSIONS}')
+    print(f'UNKNOWN: {UNKNOWN}')
+
+
+
---
+++ main.py
@@ -0,0 +1,54 @@
+from pathlib import Path
+import shutil
+import sys
+import file_parser
+from normalize import normalize
+
+def handle_media(file_name: Path, target_folder: Path):
+    target_folder.mkdir(exist_ok=True, parents=True)
+    file_name.replace(target_folder / normalize(file_name.name))
+
+def handle_archive(file_name: Path, target_folder: Path):
+    target_folder.mkdir(exist_ok=True, parents=True)
+    folder_for_file = target_folder / normalize(file_name.name.replace(file_name.suffix, ''))
+    folder_for_file.mkdir(exist_ok=True, parents=True)
+    try:
+        shutil.unpack_archive(str(file_name.absolute()), str(folder_for_file.absolute()))
+    except shutil.ReadError:
+        folder_for_file.rmdir()
+        return
+    file_name.unlink()
+
+def main(folder: Path):
+    file_parser.scan(folder)
+    for file in file_parser.GIF_IMAGES:
+        handle_media(file, folder / 'images' / 'GIF')
+    for file in file_parser.JPEG_IMAGES:
+        handle_media(file, folder / 'images' / 'JPEG')
+    for file in file_parser.JPG_IMAGES:
+        handle_media(file, folder / 'images' / 'JPG')
+    for file in file_parser.PNG_IMAGES:
+        handle_media(file, folder / 'images' / 'PNG')
+    for file in file_parser.SVG_IMAGES:
+        handle_media(file, folder / 'images' / 'SVG')
+    for file in file_parser.MP3_AUDIO:
+        handle_media(file, folder / 'audio' / 'MP3_AUDIO')
+    for file in file_parser.MY_OTHER:
+        handle_media(file, folder / 'my_other')
+    for file in file_parser.HTM_MY_DOCUMENTS:
+        handle_media(file, folder / 'documents' / 'HTM')
+    for file in file_parser.PDF_FILE:
+        handle_media(file, folder / 'PDF_FILE')
+    for file in file_parser.ARCHIVES:
+        handle_archive(file, folder / 'archives')
+
+    for folder in file_parser.FOLDERS[::-1]:
+        # Видаляємо пусті папки після сортування
+        try:
+            folder.rmdir()
+        except OSError:
+            print(f'Error during remove folder {folder}')
+
+if __name__ == "__main__":
+    folder_process = Path(sys.argv[1])
+    main(folder_process.resolve())
